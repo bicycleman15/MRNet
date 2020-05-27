b6b7dc3c0bfbeeb3b06ef2220b00ab6ab4047940
Jatin Prakash <jatinprakash1511@gmail.com>
2020-05-27 17:46:53 +0530

    final changes in model

--- models/MRnet.py
+++ models/MRnet.py
@@ -16,30 +16,36 @@ class MRnet(nn.Module):
         self.saggital = self._generate_resnet()
 
         self.fc = nn.Sequential(
-            nn.Linear(in_features=2048,out_features=1024),
+            nn.Linear(in_features=3*2048,out_features=1024),
             nn.ReLU(),
             nn.Linear(in_features=1024,out_features=2),
         )
 
     def forward(self,x): # TODO : see what to do ??
-        """ Input is given in the form of `[1, [image1, image2, image3]]` where
-        `1` is due to the dataloader assigning it a single batch. 
+        """ Input is given in the form of `[image1, image2, image3]` where
+        `image1 = [1, slices, 3, 224, 224]`. Note that `1` is due to the 
+        dataloader assigning it a single batch. 
         """
 
         # squeeze the first dimension as there
         # is only one patient in each batch
-        x = torch.squeeze(x, dim=0) 
+        images = [torch.squeeze(img, dim=0) for img in x]
 
-        x = self.body(x)
+        image1 = self.axial(images[0]).view(-1,2048)
+        image2 = self.coronal(images[1]).view(-1,2048)
+        image3 = self.saggital(images[2]).view(-1,2048)
 
-        # just a precautionary step
-        x = x.view(-1,2048) # flatten x
+        image1 = torch.max(image1,dim=0,keepdim=True)[0].squeeze(dim=0)
+        image2 = torch.max(image2,dim=0,keepdim=True)[0].squeeze(dim=0)
+        image3 = torch.max(image3,dim=0,keepdim=True)[0].squeeze(dim=0)
 
-        x = self.fc(x)
+        output = torch.cat([image1,image2,image3])
 
-        # no need to take softmax here
-        # as cross_entropy loss combines both softmax and NLL loss
-        return x
+        output = self.fc(output)
+
+        # # no need to take softmax here
+        # # as cross_entropy loss combines both softmax and NLL loss
+        return output
     
     def _generate_resnet(self):
         """make all resnet params non-trainable, called automatically
@@ -52,6 +58,7 @@ class MRnet(nn.Module):
         # remove last layer of resnet
         body = nn.Sequential(*resnet_modules[:-1])
         
+        # make params non trainable
         for x in body.parameters():
             x.requires_grad = False
 
--- temp.py
+++ temp.py
@@ -4,25 +4,27 @@ import torch
 from models import MRnet
 from dataset import MRData
 
-print("Loading Data...")
-data_train = MRData(plane='axial',task='acl')
+# print("Loading Data...")
+# data_train = MRData(plane='axial',task='acl')
 
-train_loader = torch.utils.data.DataLoader(
-        data_train, batch_size=1, shuffle=True, num_workers=11, drop_last=False)
+# train_loader = torch.utils.data.DataLoader(
+#         data_train, batch_size=1, shuffle=True, num_workers=11, drop_last=False)
 
-for x,y in train_loader:
-    print(x.shape)
-    print(y.shape)
-    break
+# for x,y in train_loader:
+#     print(x[1].shape)
+#     print(y.shape)
+#     break
 
 # image,label = data_train[0]
 
-# print("Loading Model...")
-# net = MRnet()
+print("Loading Model...")
+net = MRnet()
 
 # print(net)
 
-# image = torch.rand(20,3,224,224)
-# image = image.view(1,-1,3,224,224)
+image = torch.rand(20,3,224,224)
+image = image.view(1,-1,3,224,224)
 
-# print(net(image))
+images = [image,image,image]
+
+print(net.forward(images))
